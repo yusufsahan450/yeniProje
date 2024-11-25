9f3d3d4f56d92a788307bb4f53b09013b5277c93
yusufsahan450 <yusufsahan4627@gmail.com>
2024-11-25 18:32:01 +0300

    İSİM İLE SOY İSİM BİRLEŞTİRME

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+while True:
+    def İsimSoyisimBulma():
+        isim = input("isminizi girin:")
+        soyisim = input("soyisiminizi girin:")
+
+        isimsoyisim = isim +" "+ soyisim
+        print(isimsoyisim)
+
+    İsimSoyisimBulma()
